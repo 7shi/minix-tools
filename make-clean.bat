ad46856bc98aa1f914a2f816c0b6226bdf7bd048
7shi <7shi@live.jp>
2010-02-24 21:59:54 +0900

    add dec21140A driver (for Virtual PC 2007)

--- make-clean.bat
+++ make-clean.bat
@@ -5,6 +5,7 @@ del pe2aout\pe2aout.vcproj.*.user
 del libc\libc.vcproj.*.user
 del libsys\libsys.vcproj.*.user
 del mfs\mfs.vcproj.*.user
+del dec21140A\dec21140A.vcproj.*.user
 del test\test.vcproj.*.user
 
 rd /s /q Debug
@@ -20,5 +21,6 @@ rd /s /q "MINIX Release"
 rd /s /q "libc\MINIX Release"
 rd /s /q "libsys\MINIX Release"
 rd /s /q "mfs\MINIX Release"
+rd /s /q "dec21140A\MINIX Release"
 rd /s /q "test\MINIX Release"
 pause
