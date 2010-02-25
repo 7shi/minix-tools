76fbd75440250ccc269eea13cd3d8c5b8d907738
7shi <7shi@live.jp>
2010-02-25 15:30:39 +0900

    support msvc

--- include/varargs.h
+++ include/varargs.h
@@ -1,7 +1,11 @@
 #ifndef _VARARGS_H
 #define _VARARGS_H
 
+#ifdef __GNUC__
 #error "GCC no longer implements <varargs.h>."
 #error "Revise your code to use <stdarg.h>."
+#else
+#include <stdarg.h>
+#endif
 
 #endif
