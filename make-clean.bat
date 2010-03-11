57a35c8726976fffeb6a8f6d02258b20a98ead3a
7shi <7shi@live.jp>
2010-03-11 11:10:30 +0900

    add servers/iso9660fs

--- make-clean.bat
+++ make-clean.bat
@@ -15,6 +15,7 @@ del servers\hgfs\srv_hgfs.vcproj.*.user
 del servers\inet\srv_inet.vcproj.*.user
 del servers\init\srv_init.vcproj.*.user
 del servers\ipc\srv_ipc.vcproj.*.user
+del servers\iso9660fs\srv_iso9660fs.vcproj.*.user
 del drivers\dec21140A\drv_dec21140A.vcproj.*.user
 del commands\msh\cmd_msh.vcproj.*.user
 del commands\sh\cmd_sh.vcproj.*.user
@@ -44,6 +45,7 @@ rd /s /q "servers\hgfs\MINIX Release"
 rd /s /q "servers\inet\MINIX Release"
 rd /s /q "servers\init\MINIX Release"
 rd /s /q "servers\ipc\MINIX Release"
+rd /s /q "servers\iso9660fs\MINIX Release"
 rd /s /q "drivers\dec21140A\MINIX Release"
 rd /s /q "commands\msh\MINIX Release"
 rd /s /q "commands\sh\MINIX Release"
